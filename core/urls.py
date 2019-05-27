74cded531abd3a3f0be2801afc2091ebf9d726c3
aleksl0l <aleksl0l@yandex.ru>
2019-05-27 12:55:48 +0300

    Change url format

--- core/urls.py
+++ core/urls.py
@@ -6,12 +6,12 @@ from core.views import SighupView
 
 joke_urls = [
     path('', views.JokeView.as_view(), name='jokes'),
-    path('/generate', views.GenerateJokeView.as_view(), name='generate_joke'),
-    path('/<int:pk>', views.JokeDetailsView.as_view(), name='detail_joke'),
+    path('generate/', views.GenerateJokeView.as_view(), name='generate_joke'),
+    path('<int:pk>/', views.JokeDetailsView.as_view(), name='detail_joke'),
 ]
 
 urlpatterns = [
-    path('login', ObtainAuthToken.as_view(), name='user_login'),
-    path('sighup', SighupView.as_view(), name='user_sighup'),
-    path('jokes', include(joke_urls)),
+    path('login/', ObtainAuthToken.as_view(), name='user_login'),
+    path('sighup/', SighupView.as_view(), name='user_sighup'),
+    path('jokes/', include(joke_urls)),
 ]
